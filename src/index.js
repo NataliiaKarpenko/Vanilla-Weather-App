f453adf17806c8722d4449cebd9cf37fd60e6a3f
Natalia Karpenko <nkarpenko1227@gmail.com>
2022-08-07 22:46:39 +0300

    Added current time to Js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 function formatDate(now) {
-let date = now.getDate();
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-let month = months[now.getMonth()];
-let year = now.getFullYear();
-return `${date} ${month}, ${year}`;
+  let date = now.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  return `${date} ${month}, ${year}`;
 }
+
+let currentDate = new Date();
+document.querySelector("#date").innerHTML = formatDate(currentDate);
+
+function formatTime(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedanesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  return `${day}, ${hour}:${minutes}`
+}
+  
 let currentTime = new Date();
-document.querySelector("#date").innerHTML = formatDate(currentTime);
+document.querySelector("#time").innerHTML = formatTime(currentTime);
