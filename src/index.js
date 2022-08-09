260851a403b133fbf710da112106fe3c39f6b994
Natalia Karpenko <nkarpenko1227@gmail.com>
2022-08-09 13:32:32 +0300

    edited the time

--- src/index.js
+++ src/index.js
@@ -34,8 +34,14 @@ function formatTime(now) {
   ];
   let day = days[now.getDay()];
   let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
   let minutes = now.getMinutes();
-  return `${day}, ${hour}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+    return `${day}, ${hour}:${minutes}`;
 }
 
 let currentTime = new Date();
