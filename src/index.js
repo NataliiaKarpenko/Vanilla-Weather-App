f1165ee9fa6634ea76993fdc7dff162ba7f900a6
Natalia Karpenko <nkarpenko1227@gmail.com>
2022-08-09 13:26:29 +0300

    set the current weather properties

--- src/index.js
+++ src/index.js
@@ -42,12 +42,19 @@ let currentTime = new Date();
 document.querySelector("#time").innerHTML = formatTime(currentTime);
 
 function displayTemperature(response) {
-  document.querySelector("#current-temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let currentTemp = document.querySelector("#current-temperature");
+  currentTemp.innerHTML = Math.round(response.data.main.temp);
+  let currentHumidity = document.querySelector("#humidity");
+  console.log(response.data);
+  currentHumidity.innerHTML = response.data.main.humidity;
+  let currentWind = document.querySelector("#wind");
+  console.log(response.data);
+  currentWind.innerHTML = response.data.wind.speed;
+  let currentWeatherConditions = document.querySelector("#weather-conditions");
+  currentWeatherConditions.innerHTML = response.data.weather[0].main;
 }
+
 let apiKey = "8dcd9f739c97fb9e5152465931cf4ba4";
-let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
