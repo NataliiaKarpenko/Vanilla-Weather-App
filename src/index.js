762f63f8e35133c86cfe88d24f397874624bf298
Natalia Karpenko <nkarpenko1227@gmail.com>
2022-08-10 22:53:06 +0300

    made the search engine function

--- src/index.js
+++ src/index.js
@@ -79,12 +79,10 @@ function displayTemperature(response) {
     "alt",
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
+
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
 }
-
-let apiKey = "8dcd9f739c97fb9e5152465931cf4ba4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
 
 function changeBackgroundImage() {
   let month = document.querySelector("#month").innerHTML;
@@ -140,3 +138,19 @@ function changeBackgroundImage() {
   }
 }
 changeBackgroundImage();
+
+function search(city) {
+  let apiKey = "8dcd9f739c97fb9e5152465931cf4ba4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
